2a0ea9ab3a5713cab1113498f564705cfde8de83
matfuentesv <mat.fuentes@duocuc.cl>
2025-01-29 18:38:31 -0300

    fiox nombre services,controller

--- src/main/java/cl/hospital/apiproduceralerts/controller/ProducerAlertController.java
+++ src/main/java/cl/hospital/apiproduceralerts/controller/ProducerAlertController.java
@@ -1,26 +1,25 @@
 package cl.hospital.apiproduceralerts.controller;
 
 import cl.hospital.apiproduceralerts.model.VitalSigns;
-import cl.hospital.apiproduceralerts.service.ProducerService;
+import cl.hospital.apiproduceralerts.service.ProducerAlertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProducerController {
+public class ProducerAlertController {
 
     @Autowired
-    private ProducerService producerService;
+    private ProducerAlertService producerAlertService;
 
 
 
     @PostMapping("/send")
     public ResponseEntity<String> sendVitalSigns(@RequestBody VitalSigns vitalSigns) {
         if (vitalSigns.isAnomalous()) {
-            producerService.sendMessage(vitalSigns);
+            producerAlertService.sendMessage(vitalSigns);
             return ResponseEntity.ok("Alerta enviada a RabbitMQ.");
         }
         return ResponseEntity.ok("Signos vitales normales.");
--- src/main/java/cl/hospital/apiproduceralerts/service/ProducerAlertService.java
+++ src/main/java/cl/hospital/apiproduceralerts/service/ProducerAlertService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProducerService {
+public class ProducerAlertService {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
