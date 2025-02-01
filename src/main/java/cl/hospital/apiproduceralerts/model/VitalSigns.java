1bf32b1ccbf0acfe2961b109b31ab682e6d77ad9
matfuentesv <mat.fuentes@duocuc.cl>
2025-01-31 23:56:14 -0300

    fix dto

--- src/main/java/cl/hospital/apiproduceralerts/model/VitalSigns.java
+++ src/main/java/cl/hospital/apiproduceralerts/model/VitalSigns.java
@@ -10,13 +10,51 @@ public class VitalSigns implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private String patientId;
-    private double heartRate;
-    private double bloodPressure;
-    private double oxygenLevel;
+    private Long pacienteId;
+    private double frecuenciaCardiaca;
+    private double presionArterial;
+    private double nivelOxigeno;
+
+    // Límites configurables
+    private static final double MIN_PRESION_ARTERIAL = 50;
+    private static final double MAX_PRESION_ARTERIAL = 120;
+    private static final double MIN_NIVEL_OXIGENO = 90;
+
+
     public boolean isAnomalous() {
-        return heartRate < 50 || heartRate > 120 || bloodPressure > 140 || oxygenLevel < 90;
+        return presionArterial < MIN_PRESION_ARTERIAL || presionArterial > MAX_PRESION_ARTERIAL
+                || nivelOxigeno < MIN_NIVEL_OXIGENO;
     }
 
+    public Long getPacienteId() {
+        return pacienteId;
+    }
 
+    public void setPacienteId(Long pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public double getFrecuenciaCardiaca() {
+        return frecuenciaCardiaca;
+    }
+
+    public void setFrecuenciaCardiaca(double frecuenciaCardiaca) {
+        this.frecuenciaCardiaca = frecuenciaCardiaca;
+    }
+
+    public double getPresionArterial() {
+        return presionArterial;
+    }
+
+    public void setPresionArterial(double presionArterial) {
+        this.presionArterial = presionArterial;
+    }
+
+    public double getNivelOxigeno() {
+        return nivelOxigeno;
+    }
+
+    public void setNivelOxigeno(double nivelOxigeno) {
+        this.nivelOxigeno = nivelOxigeno;
+    }
 }
